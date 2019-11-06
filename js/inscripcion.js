02dea974cbe6f375f1ce7d35eb8965bf4697dfa6
ConstanzaD <constanzadiaz1d@hotmail.com>
2019-11-05 22:54:42 -0300

    no anda y no sé por qué

---
+++ js/inscripcion.js
@@ -0,0 +1,14 @@
+function agregarPersona(){
+    var uno = document.getElementById("inscripcion");
+    var dos = uno.cloneNode(true);
+    document.getElementById("contenedor").appendChild(dos);
+}
+
+function eliminarPersona(){
+     var uno = document.getElementById("inscripcion");
+     var tamanio = document.getElementById("contenedor").childNodes.length;
+     if (tamanio > 3){
+     document.getElementById("contenedor").removeChild(uno);
+
+     }
+}
