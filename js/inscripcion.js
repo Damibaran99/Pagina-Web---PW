cb6038757f6bac237485434a66ef2af62ca4ed80
ConstanzaD <constanzadiaz1d@hotmail.com>
2019-11-13 14:54:16 -0300

    .

--- js/inscripcion.js
+++ js/inscripcion.js
@@ -1,16 +1,23 @@
+var a = 1890;
 function agregarPersona(){
     var uno = document.getElementById("inscripcion");
     var dos = uno.cloneNode(true);
     document.getElementById("contenedor").appendChild(dos);
-}
+    }
 
 function eliminarPersona(){
      var uno = document.getElementById("contenedor"). lastChild;
      var tamanio = document.getElementById("contenedor").childNodes.length;
      if (tamanio > 3){
      document.getElementById("contenedor").removeChild(uno);
-
      }
+}
 
+function sumarPrecio(){
+    a = a + 1890;
+    document.getElementById("precio").innerText= "$" + a;
+    }
 
+function clear(){
+    document.getElementsByClassName("primeraFila").innerText = " ";
 }
