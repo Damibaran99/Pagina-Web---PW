14f01608572935fc8f2bade7bf42fe30fa4b0e70
ConstanzaD <constanzadiaz1d@hotmail.com>
2019-11-14 00:27:54 -0300

    consulta no anda

--- js/contacto.js
+++ js/contacto.js
@@ -3,35 +3,42 @@
       var apellido = document.getElementById("apellido").value;
       var email = document.getElementById("email").value;
       var tel = document.getElementById("tel").value;
-      var consulta = document.getElementById("consulta").value;
-      expresion = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+//      var consulta = document.getSelection("consulta").value;
+      var expresion = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 
 
-      if(this.nombre == "") {
-      alert("Ingrese su nombre")
-      return false;
+      if(nombre == "") {
+        alert("Ingrese su nombre")
+            return false;
       }
 
       if(apellido == "") {
-      alert("Ingrese su apellido");
-      return false;
+        alert("Ingrese su apellido");
+            return false;
       }
 
-      if (!expresion.test(email) && email == "") {
-      alert("La dirección de correo es incorrecta.");
-      return false;
-     }
-
-     if(tel == "" && tel.length == 8){
-         alert("Ingrese su teléfono");
-     }
-
-     if(consulta == "" && consulta.length <= 1000) {
-      alert("Ingrese su consulta");
-      return false;
+      if(email == ""){
+        alert("Ingrese su email");
+          return false;
       }
+
+      if (!expresion.test(email)) {
+        alert("La dirección de correo es incorrecta");
+            return false;
+     }
+
+     if(tel.length !=0){
+     if(tel.length !=8){
+         alert("El número de teléfono debe contener 8 caracteres");
+            return false;
+        }
+     }
+
+//     if(consulta.length <= 1000) {
+//      alert("Sobrepasó el número máximo de caracteres");
+//      return false;
+//      }
 
       alert("Información enviada, nos contactaremos con usted a la brevedad");
       return true;
   }
-
